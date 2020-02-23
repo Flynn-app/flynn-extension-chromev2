a741822f2ea26c469df484320cddcde457111f7a
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-23 20:24:03 +0100

    work but issue with animation

--- build/app.js
+++ build/app.js
@@ -53609,57 +53609,35 @@ exports.tanimoto = function (a, b) {
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _postlight_mercury_parser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @postlight/mercury-parser */ "./node_modules/@postlight/mercury-parser/dist/mercury.js");
 /* harmony import */ var _postlight_mercury_parser__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_postlight_mercury_parser__WEBPACK_IMPORTED_MODULE_0__);
- // const buttonMercury = document.getElementById("mercuryButton");
-// buttonMercury.addEventListener("click", (event) => {
-// buttonMercury.innerHTML = '<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>';
-// buttonMercury.style.opacity = '0.5';
 
-console.log("START");
 var ENV = {
   SERVER_ADDRESS: 'http://127.0.0.1:3000'
-}; // function fetchData() {
-//   var toto =chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-//      function(tabs){
-//         tabs[0].url
-//      }
-//   );
-// };
-// const cors = 'https://cors-anywhere.herokuapp.com/';
-// let targetSite = fetchData();
-// const urlTarget = `${cors}${targetSite}`;
-// console.log("GET URL")
-// const extractContent = (html) => {
-//   return (new DOMParser).parseFromString(html, "text/html") .
-//       documentElement . textContent;
-// }
-
-var sendData = function sendData(data) {
-  var test = data;
-  var url = "".concat(ENV['SERVER_ADDRESS'], "/api/v1/audios");
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      "audio": {
-        "title": "".concat(data.title),
-        "text_html": "".concat(data.content),
-        "text_url": ""
-      }
-    })
-  }).then(function (response) {
-    return response.json();
-  }); // .then(rep => redirect(rep.id))
 };
-
-console.log("SEND TO FLYNN");
 chrome.tabs.query({
   'active': true,
   'windowId': chrome.windows.WINDOW_ID_CURRENT
 }, function (tabs) {
-  // alert(tabs[0].url);
+  var sendData = function sendData(data) {
+    var test = data;
+    var url = "".concat(ENV['SERVER_ADDRESS'], "/api/v1/audios");
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        "audio": {
+          "title": "".concat(data.title),
+          "text_html": "".concat(data.content),
+          "text_url": "".concat(tabs[0].url)
+        }
+      })
+    }).then(function (response) {
+      return response.json();
+    });
+  };
+
   _postlight_mercury_parser__WEBPACK_IMPORTED_MODULE_0___default.a.parse(tabs[0].url).then(function (result) {
     return sendData(result);
   });
--- static/background.js
+++ static/background.js
@@ -1,56 +1,26 @@
 import Mercury from '@postlight/mercury-parser';
 
-// const buttonMercury = document.getElementById("mercuryButton");
-// buttonMercury.addEventListener("click", (event) => {
-
-  // buttonMercury.innerHTML = '<div class="spinner-border text-light" role="status"><span class="sr-only">Loading...</span></div>';
-  // buttonMercury.style.opacity = '0.5';
-
-console.log("START")
 
 const ENV = {
   SERVER_ADDRESS: 'http://127.0.0.1:3000',
 }
 
-// function fetchData() {
-//   var toto =chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-//      function(tabs){
-//         tabs[0].url
-//      }
-//   );
-// };
-
-// const cors = 'https://cors-anywhere.herokuapp.com/';
-// let targetSite = fetchData();
-// const urlTarget = `${cors}${targetSite}`;
-
-// console.log("GET URL")
-
-// const extractContent = (html) => {
-//   return (new DOMParser).parseFromString(html, "text/html") .
-//       documentElement . textContent;
-// }
-
-const sendData = (data) => {
-  const test = data ;
-  const url = `${ENV['SERVER_ADDRESS']}/api/v1/audios`;
-    fetch(url, {
-      method: 'POST',
-      headers: { "Content-Type": "application/json",
-                 "Accept": "application/json"
-       },
-      body: JSON.stringify({ "audio": { "title": `${data.title}`,
-                                        "text_html": `${data.content}`,
-                                        "text_url": ``
-      }})
-    }).then(response => response.json())
-      // .then(rep => redirect(rep.id))
-}
-
-console.log("SEND TO FLYNN")
 chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-   function(tabs){
-      // alert(tabs[0].url);
+  function(tabs){
+    const sendData = (data) => {
+      const test = data ;
+      const url = `${ENV['SERVER_ADDRESS']}/api/v1/audios`;
+        fetch(url, {
+          method: 'POST',
+          headers: { "Content-Type": "application/json",
+                     "Accept": "application/json"
+           },
+          body: JSON.stringify({ "audio": { "title": `${data.title}`,
+                                            "text_html": `${data.content}`,
+                                            "text_url": `${tabs[0].url}`
+          }})
+        }).then(response => response.json())
+  }
       Mercury.parse(tabs[0].url).then(result => sendData(result));
    }
 );
