07a97a338deece73e936d9b6cb5c70761b643e2d
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-24 20:58:18 +0100

    add animation

--- build/index.js
+++ build/index.js
@@ -1,15 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "../app/App";
-import "./index.css";
-
-ReactDOM.render(<App />, document.querySelector("#root"));
 
 const showContent = () => {
   document.querySelectorAll('.loader').forEach( wave => {
     wave.style.display = "none";
-  } )
+  })
   document.getElementById('save').style.display = "";
 }
-
 setTimeout(showContent, 2000);
