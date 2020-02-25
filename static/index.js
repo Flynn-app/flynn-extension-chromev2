80595f357c77e2170394310a2769f2db0772c993
Nicolas Boulet <nicolasboulet@live.fr>
2020-02-25 21:44:09 +0100

    popup v2

--- static/index.js
+++ static/index.js
@@ -5,11 +5,11 @@ import "./index.css";
 
 ReactDOM.render(<App />, document.querySelector("#root"));
 
-const showContent = () => {
-  document.querySelectorAll('.loader').forEach( wave => {
-    wave.style.display = "none";
-  } )
-  document.getElementById('save').style.display = "";
-}
+// const showContent = () => {
+//   document.querySelectorAll('.loader').forEach( wave => {
+//     wave.style.display = "none";
+//   } )
+//   document.getElementById('save').style.display = "";
+// }
 
-setTimeout(showContent, 2000);
+// setTimeout(showContent, 2000);
